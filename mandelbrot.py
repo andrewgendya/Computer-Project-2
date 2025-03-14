97dee22b2234e79dbf82ae21206f5e9ab578762f
razo-alexander <149010490+razo-alexander@users.noreply.github.com>
2025-03-13 21:59:58 -0400

    get_julia_color_arr()

--- mandelbrot.py
+++ mandelbrot.py
@@ -34,6 +34,32 @@ def get_escape_time_color_arr(
     color_arr = (max_iterations-escape_time+1)/(max_iterations+1)
     return color_arr
 
+def get_julia_color_arr(c_arr: np.ndarray, c: complex, max_iterations: int) -> np.ndarray:
+    """
+    Computes julia color array for grid of complex numbers
+
+    Parameters
+    ----------
+    c_arr : np.ndarray -> 2D numpy array of complex numbers
+    c : complex number -> Julia set parameter
+    max_iterations : int -> Max number of iterations
+
+    Returns
+    -------
+    np.ndarray -> 2D numpy array of julia color arrays
+    """
+    z = np.array(c_arr, dtype=np.complex128)
+    escape_time = np.full(c_arr.shape, max_iterations + 1, dtype=np.int32)
+    mask = np.ones(c_arr.shape, dtype=np.bool) # ensure proper boolean array
+
+    for i in range(1, max_iterations + 1):
+        z[mask] = z[mask] ** 2 + c # julia iteration
+        escape=np.abs(z) > max(abs(c), 2) # escape condition
+        escape_time[mask * escape] = i # store escape time
+        mask = mask * (~escape) # update mask
+
+    color_arr = (max_iterations - escape_time + 1) / (max_iterations + 1) # changes escape times to grey scale
+    return color_arr
 
 
 
